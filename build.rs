55028dc3218d4bd5979f5f1ada7b01029e835223
Jared McQueen <jaredmcqueen@gmail.com>
2023-10-24 22:23:55 -0400

    minor updates

--- build.rs
+++ build.rs
@@ -5,18 +5,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     tonic_build::configure()
-        // .protoc_arg("--experimental_allow_proto3_optional") // for older systems
-        // .build_client(true)
-        // .build_server(true)
         .file_descriptor_set_path(out_dir.join("sherpa_descriptor.bin"))
-        // .out_dir("./src")
-        .compile(&["./proto/sherpa.proto", "proto/stock.proto"], &["proto"])?;
+        .compile(
+            &[
+                "proto/sherpa.proto",
+                "proto/stock.proto",
+                "proto/news.proto",
+                "proto/crypto.proto",
+            ],
+            &["proto"],
+        )?;
     Ok(())
 }
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     tonic_build::configure().compile(
-//         &["proto/sherpa.proto", "proto/stock.proto"],
-//         &["proto"], // specifies the folder where proto files are searched
-//     )?;
-//     Ok(())
-// }
